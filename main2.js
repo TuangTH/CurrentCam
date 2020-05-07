ef9675ae6844723c9f3832548928c50fd7794007
Tuang T <tuangstudio@gmail.com>
2020-05-07 18:11:57 +0700

    VDO plays frame based on scrolling
    video needed to be smaller size.

---
+++ main2.js
@@ -0,0 +1,47 @@
+window['ann'] = [{'at':100,'msg':'cool'},{'at':230,'msg':'coolianno'}];
+
+// select video element
+var vid = document.getElementById("v0");
+//var vid = $('#v0')[0]; // jquery option
+
+var currentFrame = $('#currentFrame');
+// var video = VideoFrame({
+//     id : 'video',
+//     frameRate: 29.97,
+//     callback : function(frame) {
+//     ann.forEach(function(ele){
+//     if (frame == ele['at']) {
+//       currentFrame.html(ele['msg']);
+//     }
+//     });
+//     }
+// });
+
+// pause video on load
+vid.pause();
+
+$('#play-pause').click(function(){
+    ChangeButtonText();
+});
+
+// pause video on document scroll (stops autoplay once scroll started)
+window.onscroll = function () {
+  vid.pause();
+};
+
+// refresh video frames on interval for smoother playback
+setInterval(function () {
+  TweenMax.to(vid, 2, { currentTime: window.pageYOffset / 400 });
+}, 40);
+
+function ChangeButtonText(){
+  if(video.video.paused){
+        video.video.play();
+        video.listen('frame');
+        $("#play-pause").html('Pause');
+    }else{
+        video.video.pause();
+        video.stopListen();
+        $("#play-pause").html('Play');
+    }
+  }
