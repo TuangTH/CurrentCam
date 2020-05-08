17c4e81ca7954d991006011fe4e141870aba4269
Tuang T <tuangstudio@gmail.com>
2020-05-08 11:33:07 +0700

    frame number on top of video file and smooth the play time
    not working

--- main.js
+++ main.js
@@ -1,78 +1,64 @@
-$(document).ready(function() {
-  // var div = $("#secTitle");
-  // var pos = div.position();
-  $(window).scroll(function() {
-    // console.log("img left: " + $(".img-area img").position().left);
-    var scrollMax = $(document).height() - $(window).height();
-    console.log("scrollTop: " + $(window).scrollTop());
-    var scroll = $(window).scrollTop();
-    // $(".img-area img").css({
-    //   width: (100 + scroll / 5) + "%"
-    // })
-    if ($(window).scrollTop() > 5 && $(window).scrollTop() <= scrollMax / 3) {
-      console.log("max scroll: " + scrollMax);
-      // if ($(".img-area img").position().left < 100) {
-      $(".main-text #title").css({
-        opacity: 100
-      });
-      $(".main-text #first").css({
-        opacity: 100
-      });
-      $(".img-area img").css({
-        width: (100 + scroll / 2) + "%"
-      });
+$(function () {
 
-    } else if ($(window).scrollTop() > scrollMax / 3 && $(window).scrollTop() <= (scrollMax - (scrollMax / 3))) {
-      console.log("spot 2");
-      $(".main-text #title").css({
-        opacity: 0
-      });
-      $(".main-text #first").css({
-        opacity: 50 - (scroll / 10)
-      });
-      $(".secTitle #second").css({
-        opacity: 100
-      });
-      $(".img-area img").clearQueue().animate({
-        left: 80 + "%"
-      });
+// select video element
+// var vid = document.getElementById("v0");
+var vid = $('#v0')[0]; // jquery option
 
-    } else if ($(window).scrollTop() > (scrollMax - (scrollMax / 3)) && $(window).scrollTop() <= scrollMax) {
-      $(".img-area img").clearQueue().animate({
-        left: 20 + "%"
-      });
-      $(".secTitle #second").css({
-        opacity: 50 - (scroll / 10)
-      });
-      $(".thdTitle #third").css({
-        opacity: 100
-      });
-    } else {
-      $(".img-area img").css({
-        width: 100 + "%"
-      });
-      $(".img-area img").clearQueue().animate({
-        left: 50 + "%"
-      });
-      $(".secTitle #second").css({
-        opacity: 0
-      });
-      $(".thdTitle #third").css({
-        opacity: 0
-      });
-      $(".main-text #first").css({
-        opacity: 0
-      });
-      $(".main-text #title").css({
-        opacity: 0
-      });
-    }
-  });
-});
+// pause video on load
+vid.pause();
+
+// pause video on document scroll (stops autoplay once scroll started)
+window.onscroll = function() {
+  vid.pause();
+  $("#time").text(vid.currentTime); // **AN issue here: time does not show up
+};
+
+//--refresh video frames on interval for smoother playback--//
+
+// Version 1 using TweenMax -- still lacking --
+// setInterval(function() {
+  // TweenMax.to(vid, 2, {
+  //   currentTime: window.pageYOffset / 400
+  // });
+// }, 40);
 
 
-// console.log("img left: " + $(".img-area img").position().left);
+// Version 2 using window.pageYOffset -- still lacking but slightly better --
+setInterval(function() {
+  if((window.pageYOffset / 400) > 3 && (window.pageYOffset / 400) < 6){
+      vid.pause();
+  } else {
 
-// $(".img-area img").clearQueue().animate({
-//   left: 50 + "%"
+      vid.currentTime = window.pageYOffset / 400;
+  }
+}, 32);
+
+//--- Text Object in JSON format ---//
+
+// window['ann'] = [{
+//   'at': 100,
+//   'msg': 'cool'
+// }, {
+//   'at': 230,
+//   'msg': 'coolianno'
+// }];
+
+//--- For Grabing frame and show text from obejct set above ---//
+// NOT WORKING YET
+
+// var currentFrame = $('#currentFrame');
+// var num;
+// var video = VideoFrame({
+//   id: 'v0',
+//   frameRate: 30,
+//   callback: function(frame) {
+//     // currentFrame.html(frame);
+//     // }
+//     ann.forEach(function(ele) {
+//       if (frame == ele['at']) {
+//         currentFrame.html(ele['msg']);
+//       }
+//     });
+//   }
 // });
+});
