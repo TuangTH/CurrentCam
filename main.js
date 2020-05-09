d3e69fc0f57a85ba6e23c432967a7f024266332a
Tuang T <tuangstudio@gmail.com>
2020-05-09 21:42:47 +0700

    initial prototype scrolling thru vid and text at fixed time
    A code structure for insert video and content in JSON file. Currently for smooth scrolling effect the video needs to be 34 seconds and under 3mb is ideal.

--- main.js
+++ main.js
@@ -1,64 +1,107 @@
-$(function () {
+//hard written JSON for testing here
+// var contentObj = [{
+//     'at': 8,
+//     'msg': 'coo00000ooooool',
+//     'title': 'title 2'
+//   },
+//   {
+//     'at': 13,
+//     'msg': 'cooliannooooooo',
+//     'title': 'title 3'
+//   }
+// ]
 
-// select video element
-// var vid = document.getElementById("v0");
+//for loading external JSON
+var contentObj = (function() {
+  var contentObj = null;
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': 'data/contents.json',
+    'dataType': "json",
+    'success': function(data) {
+      contentObj = data;
+    }
+  });
+  return contentObj;
+})();
+
+if (window.console && window.console.log) {
+  // console is available
+  console.log("console is ready.");
+}
+
+
+//$(function () {
 var vid = $('#v0')[0]; // jquery option
-
+var videoStartTime = 0;
+var durationTime = 0;
+var timeDivider = 400,
+  compareNum = 6;
+var ptag = $('#one')[0]; //    var ptag = document.getElementById('one');
+var htag = $('#title')[0];
+var k;
 // pause video on load
 vid.pause();
 
 // pause video on document scroll (stops autoplay once scroll started)
 window.onscroll = function() {
   vid.pause();
-  $("#time").text(vid.currentTime); // **AN issue here: time does not show up
+  $("#time").text(vid.currentTime);
+  durationTime = window.pageYOffset / timeDivider;
+  $("#time1").text(durationTime);
+  //An option for getting contentOnj message at specific time ('at').
+  //        if ((durationTime > contentObj[0].at) && (durationTime <= (contentObj[0].at)+1)){
+  //          ptag.innerHTML = 'Observe the Change. I have changed.';
+  //          ptag.innerHTML = contentObj[0].msg;
+  //        }
+  for (var k in contentObj) {
+    // loop through every contentObj list
+          console.log(contentObj[k]);
+    if (contentObj[k].hasOwnProperty('at')) {
+      var min = contentObj[k].at
+      console.log(min);
+      if ((min > durationTime) && (min <= durationTime + 1)) {
+        console.log(min);
+        console.log("message: " + contentObj[k].msg);
+        ptag.innerHTML = contentObj[k].msg;
+        htag.innerHTML = contentObj[k].title;
+      }
+    }
+
+  }
 };
 
-//--refresh video frames on interval for smoother playback--//
-
-// Version 1 using TweenMax -- still lacking --
-// setInterval(function() {
-  // TweenMax.to(vid, 2, {
-  //   currentTime: window.pageYOffset / 400
-  // });
-// }, 40);
-
-
-// Version 2 using window.pageYOffset -- still lacking but slightly better --
-setInterval(function() {
-  if((window.pageYOffset / 400) > 3 && (window.pageYOffset / 400) < 6){
-      vid.pause();
-  } else {
-
-      vid.currentTime = window.pageYOffset / 400;
+vid.addEventListener('timeupdate', function() {
+  $("#time").text(vid.currentTime);
+  if ((window.pageYOffset / timeDivider) >= compareNum && this.currentTime > (window.pageYOffset / timeDivider)) {
+    this.pause();
+    vid.currentTime = Math.round(window.pageYOffset / timeDivider);
+    //vid.currentTime = (window.pageYOffset / timeDivider);
+  } else if ((window.pageYOffset / timeDivider) > compareNum && this.currentTime < (window.pageYOffset / timeDivider)) {
+    this.play();
   }
-}, 32);
-
-//--- Text Object in JSON format ---//
-
-// window['ann'] = [{
-//   'at': 100,
-//   'msg': 'cool'
-// }, {
-//   'at': 230,
-//   'msg': 'coolianno'
-// }];
-
-//--- For Grabing frame and show text from obejct set above ---//
-// NOT WORKING YET
-
-// var currentFrame = $('#currentFrame');
-// var num;
-// var video = VideoFrame({
-//   id: 'v0',
-//   frameRate: 30,
-//   callback: function(frame) {
-//     // currentFrame.html(frame);
-//     // }
-//     ann.forEach(function(ele) {
-//       if (frame == ele['at']) {
-//         currentFrame.html(ele['msg']);
-//       }
-//     });
-//   }
-// });
 });
+
+
+// Another option for refreshing video frames on interval for smoother playback
+//    setInterval(function () {
+//        if ((window.pageYOffset / timeDivider) > 3 && (window.pageYOffset / timeDivider) < 6) {
+//            vid.pause();
+//             alert("vid is paused.");
+//        } else if ((window.pageYOffset / timeDivider) > 6 && (window.pageYOffset / timeDivider) < 7) {
+//            vid.play();
+//        } else if ((window.pageYOffset / timeDivider) >= 7 || (window.pageYOffset / timeDivider) < 3) {
+//            vid.currentTime = window.pageYOffset / timeDivider;
+//        }
+//    }, 32);
+//
+//});
+
+setInterval(function() {
+  TweenMax.to(vid, 2, {
+    currentTime: window.pageYOffset / timeDivider
+  });
+}, 40);
+
+//});
